9b344e87575b28fa1556cfc9715438c80f513bae
panagiotis-rousseas <161859356+panagiotis-rousseas@users.noreply.github.com>
2024-04-05 12:19:31 +0300

    Update masterchef.sql

--- masterchef.sql
+++ masterchef.sql
@@ -19,7 +19,7 @@ CREATE TABLE meal(
 );
 
 CREATE TABLE recipe(
-    recipe_id INT(20) NOT NULL,
+    recipe_id INT AUTO_INCREMENT NOT NULL,
     recipeName VARCHAR(50) NOT NULL,
     recipeCategory VARCHAR(20) NOT NULL,
     CONSTRAINT Check_YourColumn CHECK (recipeCategory IN ('Μαγειρικής', 'Ζαχαροπλαστικής')) ,
@@ -30,7 +30,7 @@ CREATE TABLE recipe(
 );
 
 CREATE TABLE ingredient(
-    ingredient_id INT(20) NOT NULL,
+    ingredient_id INT AUTO_INCREMENT NOT NULL,
     ingredientName VARCHAR(50) NOT NULL,
     unitOfMeasurement INT(20) NOT NULL,
     PRIMARY KEY(ingredient_id)
@@ -45,19 +45,28 @@ CREATE TABLE ingredient(
 ΠΟΙΑ ΚΑΙ ΠΟΣΑ ΥΛΙΚΑ ΠΕΡΙΕΧΟΝΤΑΙ ΣΕ ΠΟΙΕΣ ΣΥΝΤΑΓΕΣ
 */
 CREATE TABLE ingedient_VS_recipe(
-    recipe_id INT(20) NOT NULL,
-    ingredient_id INT(20) NOT NULL,
+    recipe_id INT  NOT NULL,
+    ingredient_id INT NOT NULL,
     PRIMARY KEY (recipe_id, ingredient_id),
     FOREIGN KEY (recipe_id) REFERENCES recipe(recipe_id),
     FOREIGN KEY (ingredient_id) REFERENCES ingredients(ingredient_id),
 
-    quantity INT(20) NULL,
+    quantity INT  NULL,
     unitOfMeasurement INT(20) NOT NULL,
     caloriesPer100ml INT(20) NOT NULL, -- Κανονικοποιηση στα 100 ml 
     calories INT(20) NOT NULL ,-- Ανα ποσοτητα που δινεται πχ 1 αυγo
 
 );
 
+
+CREATE TABLE episodes(
+   episode_id INT AUTO_INCREMENT NOT NULL,
+   episode_name VARCHAR(50) NOT NULL,
+   episode_date date not null,
+   season INT NOT NULL,
+   --winner VARCHAR(50),
+   
+);
 CREATE TABLE winner(
   episode_id INT NOT NULL,
   cookID INT NOT NULL,
