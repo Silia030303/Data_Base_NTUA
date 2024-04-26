53b621db1a9a2086a357abf474598a472c831468
eugenia-skagkou <160538614+eugenia-skagkou@users.noreply.github.com>
2024-04-26 11:38:59 +0300

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -21,9 +21,8 @@ JOIN recipe r on r.recipe_id = ecr.recipe_id
 JOIN national_cuisine n on r.natcuis_id = n.natcuis_id
 GROUP BY cook_id,natcuis_id;
 
-
 SELECT c.first_name,c.last_name
 from cook c 
 JOIN cook_nat_cuis cn on c.cook_id = cn.cook_id
 JOIN national_cuisine nc on cn.natcuis_id = nc.natcuis_id
-where nc.natcuis_name = 'Afghan';
+where nc.natcuis_name = 'Afghan cuisine';
