4f553294a6c4f7332fa2a6e542b4355f169a30d9
eugenia-skagkou <160538614+eugenia-skagkou@users.noreply.github.com>
2024-05-02 17:02:28 +0300

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -58,7 +58,10 @@ LEFT JOIN
     judge j ON c.cook_id = j.cook_id
 WHERE 
     j.judge_id IS NULL;
---------------------------------------------Query 5---------------------------------
+------------------------------------Query 5-----------------------------------
+
+
+--------------------------------------------Query 6---------------------------------
 SELECT t1.tag_name AS tag_name1, t2.tag_name AS tag_name2, COUNT(*) AS pair_count
 FROM recipe_tag rt1 
 JOIN episode_cook_recipe ecr ON rt1.recipe_id = ecr.recipe_id
@@ -68,6 +71,6 @@ JOIN tags t2 ON rt2.tag_id = t2.tag_id
 GROUP BY rt1.tag_id, rt2.tag_id
 ORDER BY pair_count DESC
 LIMIT 3;
-----------------------------------------Query 6------------------------------------
+----------------------------------------Query 7------------------------------------
 
 
