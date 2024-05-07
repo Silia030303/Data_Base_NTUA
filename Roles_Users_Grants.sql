2212cc371f8c3c113c6b46f20960a75291ec0838
eugenia-skagkou <160538614+eugenia-skagkou@users.noreply.github.com>
2024-05-08 00:44:15 +0300

    Update Roles_Users_Grants.sql

--- Roles_Users_Grants.sql
+++ Roles_Users_Grants.sql
@@ -1,1 +1,6 @@
-------------------USERS--------------------------------
+CREATE ROLE cook_role;
+GRANT INSERT on recipe to cook;
+CREATE USER 'gordon_ramsey'@'localhost' IDENTIFIED by 'password1';
+GRANT cook_role to 'gordon_ramsey'@'localhost';
+GRANT SELECT, INSERT ON masterchef.recipe TO cook;
+-- GRANT SELECT, INSERT ON masterchef.recipe TO 'gordon_ramsey'@'localhost';
