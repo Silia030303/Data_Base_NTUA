f341920b91dbaa614bae583073f525cbb3f1f116
Silia030303 <160490846+Silia030303@users.noreply.github.com>
2024-05-08 20:02:08 +0300

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -24,7 +24,7 @@ GROUP BY cook_id,natcuis_id;
 
 
 --Panos
-SELECT c.last_name, e.grade ,e.episode_id, AVG(e.grade) as aver_grade
+SELECT c.last_name, AVG(e.grade) as aver_grade
 FROM cook c
 JOIN evaluation e ON c.cook_id = e.cook_id
 JOIN episode_cook_recipe ecr ON e.cook_id = ecr.cook_id AND e.episode_id = ecr.episode_id
