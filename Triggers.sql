15d9762ca535570a8092f3745a7e1b915e9f64ff
Silia030303 <160490846+Silia030303@users.noreply.github.com>
2024-05-20 13:00:18 +0300

    Update Triggers.sql

--- Triggers.sql
+++ Triggers.sql
@@ -59,7 +59,7 @@ BEGIN
     Drop (drop),
     Shot (for liquids) (shot),
     Pack or package (pack or package)
-) '
+) ';
     END IF;
 END; //
 
@@ -94,7 +94,7 @@ BEGIN
     'Drop (drop)',
     'Shot (for liquids) (shot)',
     'Pack or package (pack or package)'
-)
+) THEN
     SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'Invalid value for unit_of_measurement_quantity. Allowed values are: (
     Gram (g),
@@ -122,7 +122,7 @@ BEGIN
     Drop (drop),
     Shot (for liquids) (shot),
     Pack or package (pack or package)
-) '    
+) '   ; 
     END IF;
 END; //
 
@@ -133,9 +133,33 @@ CREATE TRIGGER check_foodgroups_update
 BEFORE UPDATE ON  foodgroups
 FOR EACH ROW
 BEGIN
-    IF NEW.foodgroups_name  IN ('Aromatic Herbs and Essential Oils', 'Coffee, Tea, and Their Products','Preserved Foods','Sweeteners','Fats and Oils','Milk, Eggs, and Their Products','Meat and Meat Products', 'Fish and Fish Products' ,'Cereals and Their Products','Various Plant-based Foods','Products with Sweeteners', 'Various Beverages') THEN
+    IF NEW.foodgroups_name  IN (
+    'Aromatic Herbs and Essential Oils',
+    'Coffee, Tea, and Their Products',
+    'Preserved Foods',
+    'Sweeteners',
+    'Fats and Oils',
+    'Milk, Eggs, and Their Products',
+    'Meat and Meat Products',
+    'Fish and Fish Products' ,
+    'Cereals and Their Products',
+    'Various Plant-based Foods',
+    'Products with Sweeteners', 
+    'Various Beverages') THEN
         SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'Invalid value for unit_of_measurement_quantity. Allowed values are:Aromatic Herbs and Essential Oils, Coffee, Tea, and Their Products,Preserved Foods,Sweeteners,Fats and Oils,Milk, Eggs, and Their Products,Meat and Meat Products, Fish and Fish Products ,Cereals and Their Products,Various Plant-based Foods,Products with Sweeteners, Various Beverages' ;
+        SET MESSAGE_TEXT = 'Invalid value for unit_of_measurement_quantity. Allowed values are
+    Aromatic Herbs and Essential Oils, 
+    Coffee, Tea, and Their Products,
+    Preserved Foods,
+    Sweeteners,
+    Fats and Oils,
+    Milk, Eggs, and Their Products,
+    Meat and Meat Products, 
+    Fish and Fish Products ,
+    Cereals and Their Products,
+    Various Plant-based Foods,
+    Products with Sweeteners, 
+    Various Beverages' ;
     END IF;
 END; //
 
@@ -146,7 +170,19 @@ FOR EACH ROW
 BEGIN
     IF NEW.foodgroups_id IN (1,2,3,4,5,6,7,8,9,10,11,12) THEN
         SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'Invalid value for unit_of_measurement_quantity. Allowed values are: 1 = Aromatic Herbs and Essential Oils, 2 = Coffee, Tea, and Their Products, 3 = Preserved Foods,4 = Sweeteners, 5 = Fats and Oils,6 = Milk, Eggs, and Their Products, 7 = Meat and Meat Products, 8= Fish and Fish Products , 9 = Cereals and Their Products, 10 = Various Plant-based Foods, 11 = Products with Sweeteners, 12 = Various Beverages' ;
+        SET MESSAGE_TEXT = 'Invalid value for unit_of_measurement_quantity. Allowed values are:
+    1 = Aromatic Herbs and Essential Oils,
+    2 = Coffee, Tea, and Their Products, 
+    3 = Preserved Foods,
+    4 = Sweeteners, 
+    5 = Fats and Oils,
+    6 = Milk, Eggs, and Their Products, 
+    7 = Meat and Meat Products, 
+    8= Fish and Fish Products ,
+    9 = Cereals and Their Products, 
+    10 = Various Plant-based Foods, 
+    11 = Products with Sweeteners, 
+    12 = Various Beverages' ;
     END IF;
 END; //
 
