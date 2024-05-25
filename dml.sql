70dc9ee13792ccf27cc2a36ff1c8ae5ed392843d
eugenia-skagkou <160538614+eugenia-skagkou@users.noreply.github.com>
2024-05-26 00:04:01 +0300

    Update dml.sql

--- dml.sql
+++ dml.sql
@@ -205,7 +205,9 @@ INSERT INTO foodgroups (foodgroups_name, description) VALUES
 ('Cereals and Their Products', 'Covers grains and cereal-based products like bread, pasta, etc.'),
 ('Various Plant-based Foods', 'Encompasses a variety of plant-based foods like fruits, vegetables, nuts, etc.'),
 ('Products with Sweeteners', 'Includes products that contain added sweetening agents.'),
-('Various Beverages', 'Covers a range of different beverages including soft drinks, juices, etc.');
+('Various Beverages', 'Covers a range of different beverages including soft drinks, juices, etc.')
+('other foodgroup that hopefully doesnt show up in the competition', 'description');
+
 
 INSERT INTO ingredient (foodgroups_id, ingredient_name)
 VALUES 
@@ -271,7 +273,8 @@ VALUES
 (7, 'Pork'),                  -- Meat and Meat Products
 (10, 'Spinach'),              -- Various Plant-based Foods
 (7, 'Lamb'),                  -- Meat and Meat Products
-(10, 'Pumpkin'); 
+(10, 'Pumpkin')
+(13,'other ingredient'); 
 
 
 
@@ -349,7 +352,13 @@ INSERT INTO recipe (
 ('Pumpkin Soup', 'main course', 18, 63, 'Creamy pumpkin soup with a hint of nutmeg', 4, 1, 15, 20, NULL, NULL, NULL, 8, 4, 20),
 ('Cheese Pizza', 'main course', 30, 18, 'Classic cheese pizza with homemade tomato sauce', 8, 2, 20, 15, NULL, NULL, NULL, 12, 15, 40),
 ('Mango Sorbet', 'dessert', 5, 54, 'Refreshing mango sorbet made with fresh mangoes', 4, 2, 10, 0, NULL, NULL, NULL, 0, 1, 30);
+
+--INSERT INTO recipe (recipe_name, recipe_category, natcuis_id, prim_ingredient_id, recipe_description, quantity_of_servings, difficulty_level, 
+--   prep_time,  cooking_time, tip_1, tip_2,  tip_3,fat_per_portion, protein_per_portion, carbohydrate_per_portion)VALUES 
+--('New recipe', 'dessert', 5, 54, 'Refreshing mango sorbet made with fresh mangoes', 4, 2, 10, 0, NULL, NULL, NULL, 0, 1, 30);
+
 ---inserting the primary ingredients of all the recipes
+
 INSERT INTO ingredient_VS_recipe (recipe_id, ingredient_id, quantity, unit_of_measurement, calories)
 VALUES
 (1, 8, 500, 'g', 250),  -- Grilled Salmon
@@ -409,6 +418,7 @@ VALUES
 (55, 63, 300, 'g', 200),  -- Pumpkin Pie
 (56, 18, 4, 'slice', 400),  -- Cheese Platter
 (57, 54, 3, 'cup', 300);  -- Mango Smoothie
+--INSERT INTO ingredient_VS_recipe (recipe_id, ingredient_id, quantity, unit_of_measurement, calories) VALUES(58,54, 3, 'cup', 300) ;
 
 INSERT INTO ingredient_VS_recipe (recipe_id, ingredient_id, quantity, unit_of_measurement, calories)
 VALUES
