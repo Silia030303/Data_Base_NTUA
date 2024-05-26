dd5d3b42261bfa4a18095cce4301112e70b568c5
Silia030303 <160490846+Silia030303@users.noreply.github.com>
2024-05-26 20:08:43 +0300

    Update Indexes.sql

--- Indexes.sql
+++ Indexes.sql
@@ -13,50 +13,13 @@
 -- QUERY 10 :recipe_id, natcuis_id
 -- QUERY 12 :recipe_id
 
-/*CREATE INDEX idx_natcuis_id ON recipe(natcuis_id);
-CREATE INDEX idx_prim_ingredient_id ON recipe(prim_ingredient_id);*/
+
 CREATE INDEX idx_recipe_category ON recipe(recipe_category);
 CREATE INDEX idx_difficulty_level ON recipe(difficulty_level);
 
-/*CREATE INDEX idx_natcuis_id ON recipe(recipe_id);*/
 
 -----------------------------------------------------------------------------------------------------------
-/*
---2. ΠΙΝΑΚΑΣ episode_cook_recipe
-
---cook_id: Χρησιμοποιείται σε πολλές συνδέσεις.
---episode_id: Χρησιμοποιείται σε συνδέσεις και για φιλτράρισμα.
---recipe_id: Χρησιμοποιείται για συνδέσεις με τον πίνακα recipe.
--- QUERY 7 : cook_id
--- QUERY 10 :episode_id
--- QUERY 12 :recipe_id, episode_id
--- QUERY 15 :recipe_id
-
-CREATE INDEX idx_cook_id ON episode_cook_recipe(cook_id);
-CREATE INDEX idx_episode_id ON episode_cook_recipe(episode_id);
-CREATE INDEX idx_recipe_id ON episode_cook_recipe(recipe_id);
-*/
------------------------------------------------------------------------------------------------------------
---3. ΠΙΝΑΚΑΣ evaluation
---cook_id: Χρησιμοποιείται σε συνδέσεις και για φιλτράρισμα.
---judge_id: Χρησιμοποιείται για συνδέσεις.
---episode_id: Χρησιμοποιείται για συνδέσεις και φιλτράρισμα.
--- QUERY 1 :cook_id,episode_id
-
-
-/*CREATE INDEX idx_eval_cook_id ON evaluation(cook_id);
-CREATE INDEX idx_eval_judge_id ON evaluation(judge_id);
-CREATE INDEX idx_eval_episode_id ON evaluation(episode_id);*/
------------------------------------------------------------------------------------------------------------
---4.  ΠΙΝΑΚΑΣ ingredient_VS_recipe
---recipe_id: Χρησιμοποιείται σε συνδέσεις και για φιλτράρισμα.
---ingredient_id: Χρησιμοποιείται σε συνδέσεις.
--- QUERY 15 :recipe_id, ingredient_id 
-
-/*CREATE INDEX idx_ing_vs_rec_recipe_id ON ingredient_VS_recipe(recipe_id);
-CREATE INDEX idx_ing_vs_rec_ingredient_id ON ingredient_VS_recipe(ingredient_id);*/
------------------------------------------------------------------------------------------------------------
---5. ΠΙΝΑΚΑΣ cook
+--2. ΠΙΝΑΚΑΣ cook
 --last_name: Χρησιμοποιείται για φιλτράρισμα και ομαδοποίηση.
 -- QUERY 1 :cook_id,last_name
 -- QUERY 2 :cook_id,
@@ -70,52 +33,28 @@ CREATE INDEX idx_cook_last_name ON cook(first_name);
 CREATE INDEX idx_cook_first_name ON cook(last_name);
 CREATE INDEX idx_cook_age ON cook(age);
 
-/*CREATE INDEX idx_cook_id ON cook(cook_id);*/
 
 -----------------------------------------------------------------------------------------------------------
 
---6. Πίνακας national_cuisine
+--3. Πίνακας national_cuisine
 --natcuis_name: Χρησιμοποιείται για φιλτράρισμα και ομαδοποίηση.
---QUERY 1 :natcuis_id,natcuis_name, 
+--QUERY 1 :natcuis_name, 
 --QUERY 2 :natcuis_name , where nc.natcuis_name 
 
 CREATE INDEX idx_natcuis_name ON national_cuisine(natcuis_name);
 -----------------------------------------------------------------------------------------------------------
 
---7. Πίνακας tags
+--4. Πίνακας tags
 --tag_name: Χρησιμοποιείται για φιλτράρισμα και συνδέσεις με τον πίνακα recipe_tag.
 --QUERY 6:SELECT t1.tag_name...,tag_id
 
 CREATE INDEX idx_tag_name ON tags(tag_name);
 
-/*CREATE INDEX idx_tag_id ON tags(tag_id);*/
+
+
 
 -----------------------------------------------------------------------------------------------------------
---8. Πίνακας recipe_tag
---recipe_id: Χρησιμοποιείται σε συνδέσεις και για φιλτράρισμα.
---tag_id: Χρησιμοποιείται σε συνδέσεις.
-/*
-CREATE INDEX idx_recipe_tag_recipe_id ON recipe_tag(recipe_id);
-CREATE INDEX idx_recipe_tag_tag_id ON recipe_tag(tag_id);*/
------------------------------------------------------------------------------------------------------------
---9. Πίνακας recipe_equipment
---recipe_id: Χρησιμοποιείται σε συνδέσεις και για φιλτράρισμα.
---equipment_id: Χρησιμοποιείται σε συνδέσεις.
---QUERY 8 :DISTINCT re.equipment_id
-/*
-CREATE INDEX idx_recipe_equipment_recipe_id ON recipe_equipment(recipe_id);
-CREATE INDEX idx_recipe_equipment_equipment_id ON recipe_equipment(equipment_id);*/
------------------------------------------------------------------------------------------------------------
---10. Πίνακας cook_nat_cuis
---cook_id: Χρησιμοποιείται σε συνδέσεις και για φιλτράρισμα.
---natcuis_id: Χρησιμοποιείται σε συνδέσεις.
---QUERY 2 :cook_id,
-/*
-CREATE INDEX idx_cook_nat_cuis_cook_id ON cook_nat_cuis(cook_id);
-CREATE INDEX idx_cook_nat_cuis_natcuis_id ON cook_nat_cuis(natcuis_id);*/
-
------------------------------------------------------------------------------------------------------------
---11. Πίνακας episode 
+--5. Πίνακας episode 
 --season:Χρησιμοποιείται σε συνδέσεις και για φιλτράρισμα.
 --QUERY 2 :season, where e.season = 2
 --QUERY 5 :episode_id 
@@ -127,22 +66,11 @@ CREATE INDEX idx_episode_season ON episode(season);
 CREATE INDEX idx_episode_episode_name ON episode(episode_name);
 -----------------------------------------------------------------------------------------------------------
 
---12. Πίνακας judge
---judge_id:Χρησιμοποιείται σε συνδέσεις και για φιλτράρισμα.
---episode_id:Χρησιμοποιείται σε συνδέσεις και για φιλτράρισμα.
---QUERΥ 4 : WHERE j.judge_id IS NUL
---QUERΥ 5 : episode_id
-/*
-CREATE INDEX idx_judge_judge_id ON judge(judge_id);
-CREATE INDEX idx_judge_episode_id ON judge(episode_id);*/
------------------------------------------------------------------------------------------------------------
---13. Πίνακας foodgroups
---foodgroups_id:Χρησιμοποιείται σε συνδέσεις και για φιλτράρισμα.
+--6. Πίνακας foodgroups
 --foodgroups_name:Χρησιμοποιείται σε συνδέσεις και για φιλτράρισμα.
 --QUERΥ 15 : foodgroups_id, SELECT fg.foodgroups_name ,WHERE fg.foodgroups_id  IS NULL
 
 
-/*CREATE INDEX idx_judge_foodgroups_id ON foodgroups(foodgroups_id);*/
 CREATE INDEX idx_judge_foodgroups_name ON foodgroups(foodgroups_name);
 
 
